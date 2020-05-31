6b49cbb78b74c46269f9edcc2554171bd0776592
Lala <lalagg@hotmail.com>
2020-05-30 21:10:50 -0400

    generating all questions

--- Develop/app.js
+++ Develop/app.js
@@ -7,10 +7,122 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+console.log(outputPath);
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
+const employeeQuestions = [
+  { type: "input", name: "name", message: "What is the employee name" },
+  { type: "number", name: "id", message: "What is the employee id" },
+  { type: "input", name: "email", message: "What is the employee email" },
+  {
+    type: "list",
+    name: "role",
+    message: "What is the employee role",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+];
+
+function getEmployeeInformation() {
+  inquirer.prompt(employeeQuestions).then((answers) => {
+    if (answers.role === "Manager") {
+      inquirer
+        .prompt([
+          {
+            type: "number",
+            name: "officeNumber",
+            message: "What is the Manager's office number?",
+          },
+          {
+            type: "confirm",
+            name: "newemployee",
+            message: "Enter another employee?",
+          },
+        ])
+        .then((managerAnswers) => {
+          const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAnswers.officeNumber
+          );
+          employees.push(manager);
+          if (managerAnswers.newemployee) {
+            getEmployeeInformation();
+          } else {
+            generatePage();
+          }
+        });
+    } else if (answers.role === "Engineer") {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "github",
+            message: "What is the Engineer's GitHub name?",
+          },
+          {
+            type: "confirm",
+            name: "newemployee",
+            message: "Enter another employee?",
+          },
+        ])
+        .then((engineerAnswers) => {
+          const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            engineerAnswers.github
+          );
+          employees.push(engineer);
+          if (engineerAnswers.newemployee) {
+            getEmployeeInformation();
+          } else {
+            generatePage();
+          }
+        });
+    } else if (answers.role === "Intern") {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "school",
+            message: "What is the Intern's school?",
+          },
+          {
+            type: "confirm",
+            name: "newemployee",
+            message: "Enter another employee?",
+          },
+        ])
+        .then((internAnswers) => {
+          const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            internAnswers.school
+          );
+          employees.push(intern);
+          if (internAnswers.newemployee) {
+            getEmployeeInformation();
+          } else {
+            generatePage();
+          }
+        });
+    }
+  });
+}
+
+function generatePage() {
+  const htmlPage = render(employees);
+
+  fs.writeFile(outputPath, htmlPage, () => {
+    console.log("employee page generated");
+  });
+}
+
+getEmployeeInformation();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
